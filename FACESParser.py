73ca0a741230156101900ecac9d6578022513712
Jerry Jeyachandra <jerrold.jeyachandra@gmail.com>
2021-01-25 14:24:47 -0500

    remove unnecessary dependencies and run yapf

--- FACESParser.py
+++ FACESParser.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 """FACES Task Parser
 
 Usage: FACESParser.py <eprime_filename> <destination>
@@ -11,113 +10,169 @@ Originally written by Ella Wiljer, 2018
 BIDS-ified by Gabi Herman, 21 Nov 2018
 """
 from docopt import docopt
-import sys
 import codecs
-import pdb
 import pandas as pd
 import numpy as np
 import re
 import os
 
+
 # function to read the eprime file
 def read_eprime(eprimefile):
-    eprime = codecs.open(eprimefile, "r", encoding = "utf-16", errors ="strict")
+    eprime = codecs.open(eprimefile, "r", encoding="utf-16", errors="strict")
     lines = []
     for line in eprime:
         lines.append(str(line))
     return lines
 
-#find all data entries in one dataset
-def find_all_data (eprime, tag):
-    dataset = [(i,s) for i,s in enumerate(eprime) if tag in s]
+
+# find all data entries in one dataset
+def find_all_data(eprime, tag):
+    dataset = [(i, s) for i, s in enumerate(eprime) if tag in s]
     return dataset
+
 
 def findnum(ln):
     try:
         txtnum = re.findall('(\d+)\r\n', ln)
         return float(txtnum[0])
-    except:
-        return "n/a"
+    except ValueError:
+        return txtnum[0]
+
+
+def get_event_value(eprime, event):
+
+    events = find_all_data(eprime, event)
+    if not events:
+        return np.nan
+    elif len(events) > 1:
+        print("Was expecting only one event, but received multiple!")
+        raise ValueError
+
+    return findalphanum(events[0][1])
+
+
+def event_is_empty(e):
+    return e.strip().endswith(':')
+
+
+def findalphanum(ln):
+
+    # Check if any values exist
+    if event_is_empty(ln):
+        return np.nan
+    try:
+        txtnum = re.findall('(?<=: )[A-Za-z0-9]+', ln)[0]
+    except IndexError:
+        print(f"Value found is unexpected!: {ln}")
+        raise
+    return txtnum
+
+
+def get_event_response(eprime, rsp_event):
+    resp = get_event_value(eprime, rsp_event)
+    return map_response(resp)
+
+
+def map_response(x):
+    '''
+    Some FACES task files use "c" and "d" instead of 1,2
+    '''
+
+    if pd.isnull(x):
+        return x
+
+    if x.isdigit():
+        return x
+
+    mapdict = {'c': 1, 'd': 2}
+    try:
+        res = int(mapdict[x])
+    except KeyError:
+        print("Value is not numeric or matches 'c or d'")
+        print(f"Value found: {x}")
+
+    return res
+
 
 def main():
 
-    arguments       = docopt(__doc__)
-    eprimefile      = arguments['<eprime_filename>']
-    destination     = arguments['<destination>']
+    arguments = docopt(__doc__)
+    eprimefile = arguments['<eprime_filename>']
+    destination = arguments['<destination>']
 
+    print(f"Parsing faces for {eprimefile}")
     text_file = eprimefile
-    print(text_file) #temporary for debugging
 
-    mr_id = eprimefile[eprimefile.find('OPT01'):eprimefile.find('OPT01')+23] #I assume there's a datman way to do this. ALso need a datman way to find the behav files in the code that calls this
+    mr_id = eprimefile[eprimefile.find('OPT01'):eprimefile.find('OPT01') + 23]
 
-
-    # it gives list index out of range errors for invalid files - i should probably build in checks for that and then make a task file "blacklist" type thing
     eprime = read_eprime(eprimefile)
 
-    #tag the trials to obtain the data for each trial
-    taglist = find_all_data(eprime,"Procedure: TrialsPROC\r\n") #that finds the trial numbers which actually have stimuli
+    # tag the trials to obtain the data for each trial
+    taglist = find_all_data(eprime, "Procedure: TrialsPROC\r\n")
 
-    #obtain the base onset time for the dataset
-    basetime_tag = find_all_data (eprime, 'SyncSlide.OnsetTime:')  #onset time of presentation of current trial in ms from script startup (according to HCP). The first one of these should be the time of the initial TR.
-    basetime = findnum(basetime_tag[0][1])
+    trial_start = np.empty([len(taglist)], dtype=int)
+    trial_end = np.empty([len(taglist)], dtype=int)
 
-    #Create variables to hold the accuracy averages for the two trial types
-    shapes_avg = 0
-    shapes_num = 0
-    faces_avg = 0
-    faces_num = 0
-    trial_start=np.empty([len(taglist)], dtype=int)
-    #trial_block=np.empty([len(taglist)])
-    trial_end=np.empty([len(taglist)], dtype=int)
-    #loop to grab the data from each trial and output it in tsv format
     for i, ind_trial_proc in enumerate(taglist):
-        #grab the data slice for each trial
-        if (i < (len(taglist))-1):
-            trial_end[i] = taglist[i+1][0]
-        elif (i == (len(taglist))-1):
-            trial_end[i] = len(eprime)
+        if (i < (len(taglist)) - 1):
+            trial_end[i] = taglist[i + 1][0]
+        elif (i == (len(taglist)) - 1):
+            trial_end[i] = len(eprime) - 1
+
         trial_start[i] = ind_trial_proc[0]
 
-    trial_blocks=[eprime[trial_start[i]:trial_end[i]] for i in range(len(trial_end))]
+    trial_blocks = [eprime[s:e] for s, e in zip(trial_start, trial_end)]
 
-    onset_times=[((findnum(find_all_data(trial_blocks[i], 'StimSlide.OnsetTime:')[0][1])-basetime)/1000) for i in range(len(trial_end))]
+    entries = []
+    for b in trial_blocks:
+        entries.append({
+            'onset':
+            get_event_value(b, 'StimSlide.OnsetTime:'),
+            'duration':
+            get_event_value(b, 'StimSlideOnsetToOnsetTime:'),
+            'trial_type':
+            'Shapes' if 'Shape' in str(b) else 'Faces',
+            'response_time':
+            get_event_value(b, 'StimSlide.RT:'),
+            'accuracy':
+            get_event_value(b, 'StimSlide.ACC:'),
+            'correct_response':
+            map_response(get_event_value(b, 'CorrectResponse:')),
+            'participant_response':
+            map_response(get_event_value(b, 'StimSlide.RESP:'))
+        })
 
-    trial_types =[('Shapes' if 'Shape' in str(trial_blocks[i]) else 'Faces') for i in range(len(trial_blocks))]
+    data = pd.DataFrame.from_dict(entries).astype({
+            "onset": np.float,
+            "duration": np.float,
+            "response_time": np.float,
+            "accuracy": np.float,
+            "correct_response": np.float,
+            "participant_response": np.float
+            })\
+            .astype({
+                "correct_response": "Int64",
+                "participant_response": "Int64",
+                "accuracy": "Int64"
+            })
 
-    RTs=[(findnum(find_all_data(eprime[trial_start[i]:trial_end[i]], 'StimSlide.RT:')[0][1])/1000) for i in range(len(trial_end))]
+    log_head, log_tail = os.path.split(eprimefile)
 
-    durations=[((findnum(find_all_data(eprime[trial_start[i]:trial_end[i]], 'StimSlide.OnsetToOnsetTime')[0][1]))/1000) for i in range(len(trial_end))]
-
-    accuracy=[int(findnum(find_all_data(eprime[trial_start[i]:trial_end[i]], 'StimSlide.ACC:')[0][1])) for i in range(len(trial_end))]
-
-    correct_response=[int(findnum(find_all_data(eprime[trial_start[i]:trial_end[i]], 'CorrectResponse:')[0][1])) for i in range(len(trial_end))]
-
-    participant_response = [findnum(find_all_data(eprime[trial_start[i]:trial_end[i]], 'StimSlide.RESP:')[0][1]) for i in range(len(trial_end))] #the problem w making this an integer is that it cant work on the n/as :(
-
-    data_list = {'onset': onset_times,'duration': durations,'trial_type': trial_types, 'response_time':RTs, 'accuracy': accuracy, 'correct_response': correct_response, 'participant_response': participant_response}
-    data = pd.DataFrame(data_list)
-
-
-    log_head, log_tail =os.path.split(eprimefile)
-
-    find=re.compile('FACES_eprime_parser\/(OPT01[^\/]*)')
+    find = re.compile('FACES_eprime_parser\/(OPT01[^\/]*)')
     m = find.findall(log_head)
-    find2=re.compile('(part\d).log')
+    find2 = re.compile('(part\d).log')
     n = find2.findall(log_tail)
-    if m:
-        sub_id=m[0]
-    else:
-        sub_id="NULL"
+    sub_id = os.path.basename(log_head)
 
-    file_name='/projects/gherman/FACES_eprime_parser/out/{}/{}_FACES.tsv'.format(sub_id, sub_id)
+    file_name = '{}/{}_FACES.tsv'.format(destination, sub_id)
 
     if not os.path.exists(os.path.dirname(file_name)):
         os.makedirs(os.path.dirname(file_name))
 
-
     #change it to proper BIDS naming
     data.to_csv(file_name, sep='\t', index=False)
 
+
 if __name__ == '__main__':
-    arguments = docopt(__doc__)
     main()
